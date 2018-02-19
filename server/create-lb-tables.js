8c77c363b623b3ffd229d6f4f076971f036df58f
Nitish Kumar <nitish@ironsystems.com>
2018-02-19 23:18:22 +0530

    adding models

---
+++ server/create-lb-tables.js
@@ -0,0 +1,18 @@
+var server = require('./server');
+var ds = server.dataSources.db;
+var lbTables = [
+  'users',
+  'contacts',
+  'groups',
+  'groupContacts',
+  'templates',
+  'sms',
+  'smsReceipts',
+  'emails',
+  'emailReceipts'
+];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
